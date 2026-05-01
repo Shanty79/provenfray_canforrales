6116c664c3bdd7d410e24b0fd654fd1b345305c0
Shanty79 <scgarcia79@gmail.com>
2026-05-01 10:52:01 +0200

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -4,7 +4,8 @@ module.exports = async (req, res) => {
 
   try {
     const response = await fetch(
-    `https://generativelanguage.googleapis.com/v1/models/gemini-1.5-flash:generateContent?key=${apiKey}`,
+    //`https://generativelanguage.googleapis.com/v1/models/gemini-1.5-flash:generateContent?key=${apiKey}`,
+    `https://generativelanguage.googleapis.com/v1/models/gemini-pro:generateContent?key=${apiKey}`,
       {
         method: "POST",
         headers: { "Content-Type": "application/json" },
